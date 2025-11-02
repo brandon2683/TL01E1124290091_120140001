6f6cca61091294f6a920ef52246eeb9235ab531a
brandon <diasbrandon268@gmail.com>
2025-11-02 14:10:31 -0600

    modificacion diseño de la segunda activiy

--- app/src/main/java/com/example/tl01e1124290091_120140001/ActivitySegunda.java
+++ app/src/main/java/com/example/tl01e1124290091_120140001/ActivitySegunda.java
@@ -1,6 +1,9 @@
 package com.example.tl01e1124290091_120140001;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ActivitySegunda extends AppCompatActivity {
 
+    Button btnvolver,btncompartir, btnimagen, btneliminar, btnactualizar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_segunda);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnvolver = (Button) findViewById(R.id.btnvolver);
+        btncompartir = (Button) findViewById(R.id.btncompartir);
+        btnimagen = (Button) findViewById(R.id.btnimagen);
+        btneliminar = (Button) findViewById(R.id.btneliminar);
+        btnactualizar = (Button) findViewById(R.id.btnactualizar);
+
+        btnvolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivitySegunda.this,MainActivity.class);
+                startActivity(intent);
+            }
         });
+
     }
 }
